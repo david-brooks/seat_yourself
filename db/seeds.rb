3deba01635898280c75c8aeeb94cfd5d7327e925
David Brooks <davidbrooks.now@gmail.com>
2014-02-09 12:16:38 -0500

    models, associations, seeds, css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = [
+  "Fast Food",
+  "Italian",
+  "Pub fare",
+  "Fine Dining",
+  "Pizza",
+  "Chinese",
+  "Steak"
+]
+
+categories.each do |name| 
+  Category.create(:name => name)
+end
+
+fast = Category.where(:name => "Fast Food").first
+pizza = Category.where(:name => "Pizza").first
+
+Restaurant.create(
+  :name => "Burito Boyz",
+  :address => "254 Adelaide St.",
+  :phone => "",
+  :description => "Whoppers!",
+  :category => fast,
+  :capacity => 1
+)
+
+Restaurant.create(
+  :name => "Jollibee",
+  :address => "100 Manilla Philipines St W",
+  :phone => "555-1212",
+  :description => "Burgers!",
+  :category => fast,
+  :capacity => 1
+)
+
+Restaurant.create(
+  :name => "Korean Barbecue",
+  :address => "123 Seoul St.",
+  :phone => "967-1111",
+  :description => "Chewy seaweed!",
+  :category => Fine Dining,
+  :capacity => 1
+)
+
+Restaurant.create(
+  :name => "Tao Chinese Buffett",
+  :address => "789 Bejing St.",
+  :phone => "333-3333",
+  :description => "Yummy Chinese food yummmm!",
+  :category => Chinese,
+  :capacity => 1
+)
